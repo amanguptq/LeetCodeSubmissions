077a2aea0a4bf856b52d012b1eaa86096e31a974
Aman Gupta <47669355+amanguptq@users.noreply.github.com>
2026-04-06 23:48:15 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cs
@@ -0,0 +1,16 @@
+// Last updated: 4/6/2026, 11:48:14 PM
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+            
+    int l=0, r = nums.Length-1;
+        while(l<=r)
+        {
+            int m = l+((r-l)/2);
+            if(nums[m]>target) {r = m -1;}
+            else if(nums[m]<target) {l=m+1;}
+            else return m;
+        }
+        return -1;
+    }
+}

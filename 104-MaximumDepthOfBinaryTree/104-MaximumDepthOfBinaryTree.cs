f5802116cdd3f04c97e65dc31bad064150c705bd
Aman Gupta <47669355+amanguptq@users.noreply.github.com>
2026-04-06 23:48:43 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ 104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,23 @@
+// Last updated: 4/6/2026, 11:48:42 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        if(root == null)
+        return 0;
+
+     return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        
+    }
+}

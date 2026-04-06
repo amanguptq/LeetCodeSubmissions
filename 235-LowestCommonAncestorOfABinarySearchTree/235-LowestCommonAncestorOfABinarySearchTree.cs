3facf126f710b8c468a627ab374e22fff25e73e9
Aman Gupta <47669355+amanguptq@users.noreply.github.com>
2026-04-06 23:48:26 +0530

    Create 235-LowestCommonAncestorOfABinarySearchTree

---
+++ 235-LowestCommonAncestorOfABinarySearchTree/235-LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,25 @@
+// Last updated: 4/6/2026, 11:48:25 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    while (root != null) {
+        if (p.val < root.val && q.val < root.val)
+            root = root.left;
+        else if (p.val > root.val && q.val > root.val)
+            root = root.right;
+        else
+            return root;
+    }
+    return null;
+
+    }
+}
